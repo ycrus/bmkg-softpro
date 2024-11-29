eaa285009ca41f69464c6366f4ef68f186c4be5c
yohanes14 <ycrusc@ist.id>
2024-11-29 13:51:25 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local') && config('clockwork.enable')) {
+            $this->app->register(\Clockwork\Support\Laravel\ClockworkServiceProvider::class);
+        }
     }
 
     /**
