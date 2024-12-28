2aab3ab91e8164777e40b138abdf5e90c94b3180
ycrusc <ycrusc@gmail.com>
2024-12-28 10:17:16 +0700

    add jenis kunjungan

--- app/Http/Controllers/AsuransiController.php
+++ app/Http/Controllers/AsuransiController.php
@@ -44,6 +44,7 @@ class AsuransiController extends Controller
             'lokasi' => 'required',
             'latitude' => 'required',
             'longitude' => 'required',
+            'kejadian'=> 'required',
         ]);
 
         $validated['user_id'] = Auth::id();
